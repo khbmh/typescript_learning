2f0ed52e98e1864a14ecad95e08395bedf150049
khbmh (Mahi) <khaledbinmonowarhossain@gmail.com>
2025-05-01 21:58:41 +0600

    basics done tuples, enums, any unknown void never

--- app.ts
+++ app.ts
@@ -1,6 +1,72 @@
-let a = 12;
-a = 22;
 
-function hello (name){
-  return `hello, ${name}`
+// basic types 
+/*
+-- primitives
+number 
+string 
+boolean
+
+-- reference
+object, array, function () {} []
+
+--
+tuples
+enums - enumeration
+any, unknown, void, null, undefined, never
+*/
+
+/*
+let z = 'name'
+let b = true;
+
+let arr = [1, 2, 4, 'hello', { name: 'Alex' }]
+let arr2: number[] = [1, 2, 4]
+console.log(z);
+
+// tuples - fixed types for array items
+let arr3: [number, string] = [3, 'hello']
+
+// enum - predefined rules or values for company or project
+enum UserRoles {
+  ADMIN = 'ceo',
+  mahi = 'manager'
 }
+
+UserRoles.ADMIN
+
+// any, unknown, void, null, undefined, never
+
+let a; //any  - it shouldn't be used normally - works like js
+
+let b3: unknown; // -- while working need to define types or give error
+
+a = 2
+a = 'hello'
+b3 = 2
+b3 = 'hello'
+
+a.toUpperCase()
+
+if (typeof b3 == 'string')
+  b3.toUpperCase()
+
+
+// void - not returning anything
+function abcd(): void {
+  console.log(
+    'hello'
+  );
+}
+let myName: string | null
+myName = 'M'
+
+// never - won't let following codes to execute - may not need in implementation
+function neverRun(): never {
+  while (true) {
+    console.log('th');
+  }
+}
+neverRun()
+console.log('never is in');
+
+*/
